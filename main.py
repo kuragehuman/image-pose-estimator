ef19950aa2e81e2114be826d422aa5b3b1047e00
kuragehuman <kuragehuman@gmail.com>
2026-03-14 15:38:06 +0900

    左右反転・3Dボーン表示

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import cv2
 import mediapipe as mp
 import matplotlib.pyplot as plt
 
-
-import mediapipe
-print(mediapipe)
-print(mediapipe.__file__)
-
 mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
+
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
 
 with mp_pose.Pose(static_image_mode=False) as pose:
 
@@ -19,22 +19,48 @@ with mp_pose.Pose(static_image_mode=False) as pose:
         if not ret:
             break
 
+        frame = cv2.flip(frame, 1)
+
         image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         result = pose.process(image_rgb)
+
+        if result.pose_landmarks:
+
+            # 2D骨格をカメラ画像に描画
+            mp_drawing.draw_landmarks(
+                frame,
+                result.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS
+            )
 
         if result.pose_world_landmarks:
 
             landmarks = result.pose_world_landmarks.landmark
 
-            xs = [lm.x for lm in landmarks]
-            ys = [lm.y for lm in landmarks]
-            zs = [lm.z for lm in landmarks]
+            xs = [-lm.x for lm in landmarks]
+            ys = [lm.z for lm in landmarks]
+            zs = [-lm.y for lm in landmarks]
 
-            plt.clf()
+            ax.cla()
+            ax.view_init(elev=10, azim=90)
 
-            ax = plt.axes(projection="3d")
             ax.scatter(xs, ys, zs)
+
+            # ボーンを描く
+            for connection in mp_pose.POSE_CONNECTIONS:
+                start = connection[0]
+                end = connection[1]
+
+                x = [xs[start], xs[end]]
+                y = [ys[start], ys[end]]
+                z = [zs[start], zs[end]]
+
+                ax.plot(x, y, z)
+
+            ax.set_xlim(-1, 1)
+            ax.set_ylim(-1, 1)
+            ax.set_zlim(-1, 1)
 
             plt.pause(0.001)
 
